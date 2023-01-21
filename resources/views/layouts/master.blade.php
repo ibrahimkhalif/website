4e99df93bd93870eee574496699a778971854d66
ibrahim <ibrahimkhalif5@gmail.com>
2023-01-21 06:29:14 +0300

    token

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,6 +9,7 @@
     <title>Najdah</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- Favicons -->
     <link href="assets/img/logo.png" rel="icon">
@@ -304,7 +305,13 @@
     });
     </script>
 
-
+    <script>
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+    </script>
 
 </body>
 
