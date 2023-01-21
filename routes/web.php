dc6f8ba164b1f40eed4165950c3841648d7e8d60
ibrahim <ibrahimkhalif5@gmail.com>
2023-01-21 07:04:12 +0300

    en

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use App\Http\Controllers\ProjectCategoryController;
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
 });
-Route::redirect('/', '/en');
+//Route::redirect('/', '/en');
 
 Route::group(['prefix' => '{locale}', 'middleware' => 'setLocale'], function () {
    
